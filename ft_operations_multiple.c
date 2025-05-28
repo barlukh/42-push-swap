fe14a1801eacbbe4624e05a418c493a71e902b1b
Boris Gazur <barlukh@gmail.com>
2025-05-28 18:39:15 +0300

    Initial commit

---
+++ ft_operations_multiple.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_operations_multiple.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 17:59:27 by bgazur            #+#    #+#             */
+/*   Updated: 2025/05/28 18:29:37 by bgazur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_config.h"
+
+int ft_ops_multiple_reverse(t_stacks *stack)
+{
+	return (SUCCESS);
+}
+
+int ft_ops_multiple_rotate(t_stacks *stack)
+{
+	return (SUCCESS);
+}
+
+int	ft_ops_multiple_swap(t_stacks *stack)
+{
+	if (stack->size_a >= 2)
+	{
+		stack->temp = stack->a[0];
+		stack->a[0] = stack->a[1];
+		stack->a[1] = stack->temp;
+	}
+	if (stack->size_b >= 2)
+	{
+		stack->temp = stack->b[0];
+		stack->b[0] = stack->b[1];
+		stack->b[1] = stack->temp;
+	}
+}
---
+++ ft_operations_single.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_operations_single.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 18:00:00 by bgazur            #+#    #+#             */
+/*   Updated: 2025/05/28 18:19:30 by bgazur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_config.h"
+
+int	ft_ops_single_push(int operation, t_stacks *stack)
+{
+	return (SUCCESS);
+}
+
+int ft_ops_single_reverse(int operation, t_stacks *stack)
+{
+	return (SUCCESS);
+}
+
+int	ft_ops_single_rotate(int operation, t_stacks *stack)
+{
+	return (SUCCESS);
+}
+
+int	ft_ops_single_swap(int operation, t_stacks *stack)
+{
+	if (operation == SA)
+	{
+		if (stack->size_a >= 2)
+		{
+			stack->temp = stack->a[0];
+			stack->a[0] = stack->a[1];
+			stack->a[1] = stack->temp;
+		}
+	}
+	else
+	{
+		if (stack->size_b >= 2)
+		{
+			stack->temp = stack->b[0];
+			stack->b[0] = stack->b[1];
+			stack->b[1] = stack->temp;
+		}
+	}
+	return (SUCCESS);
+}
