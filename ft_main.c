549e20fa0c589e09c0b350a831b413ec0e10c4d3
Boris Gazur <barlukh@gmail.com>
2025-05-29 18:17:21 +0300

    Implement a raw prototype of a custom merge sort

--- ft_main.c
+++ ft_main.c
@@ -6,13 +6,11 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 17:24:31 by bgazur            #+#    #+#             */
-/*   Updated: 2025/05/29 10:42:44 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/05/29 12:15:04 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_config.h"
-
-static void	ft_ops_main(int operation, t_stacks *stack);
 
 int	main(int argc, char **argv)
 {
@@ -24,17 +22,18 @@ int	main(int argc, char **argv)
 	arg.offset = TRUE;
 	if (ft_parse_input(&arg, &stack) == ERROR)
 		return (ERROR);
+	ft_sort(&stack);
 	// REMEMBER TO REMOVE BEFORE SUBMISSION!
-	// ft_ops_main(RA, &stack);
-	ft_ops_main(PB, &stack);
-	ft_ops_main(PB, &stack);
-	ft_ops_main(PB, &stack);
-	ft_ops_main(PB, &stack);
+	// ft_ops_main(SA, &stack);
 	// ft_ops_main(PB, &stack);
-	// ft_ops_main(SB, &stack);
-	// ft_ops_main(SB, &stack);
-	ft_ops_main(RRB, &stack);
-	// ft_ops_main(RR, &stack);
+	// ft_ops_main(PB, &stack);
+	// ft_ops_main(PB, &stack);
+	// ft_ops_main(RA, &stack);
+	// ft_ops_main(RB, &stack);
+	// ft_ops_main(RRA, &stack);
+	// ft_ops_main(RRB, &stack);
+	// ft_ops_main(SA, &stack);
+	// ft_ops_main(PA, &stack);
 	// ft_ops_main(PA, &stack);
 	// ft_ops_main(PA, &stack);
 	// ft_ops_main(SS, &stack);
@@ -54,25 +53,4 @@ int	main(int argc, char **argv)
 	if (arg.offset == TRUE)
 		return (ft_end(FREE_AB, arg.argv, &stack));
 	return (ft_end(FREE_ARG_AB, arg.argv, &stack));
-}
-
-// Calls relevant function based on the operation passed
-void	ft_ops_main(int operation, t_stacks *stack)
-{
-	if (operation == RRR)
-		ft_ops_multiple_reverse(stack);
-	else if (operation == RR)
-		ft_ops_multiple_rotate(stack);
-	else if (operation == SS)
-		ft_ops_multiple_swap(stack);
-	else if (operation == PA)
-		ft_ops_single_push_a(stack);
-	else if (operation == PB)
-		ft_ops_single_push_b(stack);
-	else if (operation == RRA || operation == RRB)
-		ft_ops_single_reverse(operation, stack);
-	else if (operation == RA || operation == RB)
-		ft_ops_single_rotate(operation, stack);
-	else
-		ft_ops_single_swap(operation, stack);
 }
--- ft_sort.c
+++ ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 10:49:14 by bgazur            #+#    #+#             */
-/*   Updated: 2025/05/29 15:49:22 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/05/29 18:13:15 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ static void	ft_ops_main(int operation, t_stacks *stack);
 
 void	ft_sort(t_stacks *stack)
 {
+	size_t	flag;
+
+	flag = 0;
 	stack->j = 0;
 	stack->pivot = stack->size_a / 2;
 	while (stack->j < stack->pivot)
@@ -23,6 +26,34 @@ void	ft_sort(t_stacks *stack)
 		ft_ops_main(PB, stack);
 		stack->j++;
 	}
+	stack->j = 0;
+	while (stack->j < 1000)
+	{
+		// if (stack->a[0] > stack->a[1] && stack->b[0] < stack->b[1])
+		// {
+		// 	ft_ops_main(SS, stack);
+		// 	flag = 0;
+		// }
+		if (stack->a[0] > stack->a[1] && stack->a[0] != 9) // Hardcoded largest value of stack 'a'
+		{	
+			ft_ops_main(SA, stack);
+			flag = 0;
+		}
+		else if (stack->b[0] < stack->b[1] && stack->b[0] != 1) // Hardcoded smallest value of stack 'b'
+		{
+			ft_ops_main(SB, stack);
+			flag = 0;
+		}
+		ft_ops_main(RRR, stack);
+		stack->j++;
+		flag++;
+		if (flag > stack->pivot)
+			break ;
+	}
+	while (stack->b[0] != 4) // Hardcoded largest values of stack 'b'
+		ft_ops_main(RB, stack);
+	while (stack->size_b != 0)
+			ft_ops_main(PA, stack);
 }
 
 // Calls relevant function based on the operation passed
